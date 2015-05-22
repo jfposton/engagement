cf024afffde13c864d104c3b4da396053ab0f0d6
Jonathan Poston <jfposton2@gmail.com>
2015-05-21 21:30:06 -0400

    Updated Gruntfile to minify html and css and allow for building and compressing the code for deploy purposes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,79 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-        pkg: {
-            name: "toast-to-poston"
-        },
+        pkg: grunt.file.readJSON('package.json'),
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
                 src: 'js/*.js',
-                dest: 'js/<%= pkg.name %>.min.js'
+                dest: 'dist/js/<%= pkg.name %>.min.js'
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html':'index.html'
+                }
+            }
+        },
+        jshint: {
+            sitejs: "js/*.js"
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+            },
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'dist/css',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {expand: true, src: ['resources/**'], dest: 'dist/'},
+                    {expand: false, src: ['favicon.ico', '.bowerrc', 'bower.json'], dest: 'dist/'}
+                ]
+            }
+        },
+        compress: {
+            main: {
+                options: {
+                    archive: 'toast-to-poston-<%= pkg.version %>.zip',
+                    mode: 'zip'
+                },
+                files: [
+                    {src: ['dist/**'], dest: ''}
+                ]
             }
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+    var npmTasks = [
+        'grunt-contrib-uglify',
+        'grunt-contrib-jshint',
+        'grunt-contrib-htmlmin',
+        'grunt-contrib-cssmin',
+        'grunt-contrib-compress',
+        'grunt-contrib-copy'
+    ]
+
+    for(i = 0; i < npmTasks.length; i++) {
+        grunt.loadNpmTasks(npmTasks[i]);
+    }
+
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('QA', ['jshint']);
+    grunt.registerTask('build', ['uglify', 'htmlmin', 'cssmin', 'copy']);
+    grunt.registerTask('deploy', ['build', 'compress']);
 };
