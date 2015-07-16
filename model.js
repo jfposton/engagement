d53643e606988140178ecfb94b61bc9de94314f3
Jonathan Poston <jfposton2@gmail.com>
2015-07-15 23:15:43 -0400

    Create a model js file
    This file will hold the model information for this website.

---
+++ model.js
@@ -0,0 +1,19 @@
+module.exports = {
+    head: {
+        title: "Toast to Poston",
+        link: [
+            "stylesheets/site.css",
+            "vendor/bootstrap/dist/css/bootstrap.min.css",
+            "vendor/animate.css/animate.min.css",
+        ]
+    },
+    scripts: [
+        "vendor/jquery/dist/jquery.min.js",
+        "vendor/bootstrap/dist/js/bootstrap.min.js",
+        "vendor/stellar.js/jquery.stellar.min.js",
+        "https://maps.googleapis.com/maps/api/js?key=AIzaSyDYeAbSkSOLay6SjLAAldJlOPpKjdktwF4",
+        "vendor/headroom.js/dist/headroom.min.js",
+        "javascripts/site.js",
+        "javascripts/map.js"
+    ]
+}
