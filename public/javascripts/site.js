1da94184b96681e7fe829f904ba92a1df010d120
Jonathan Poston <jfposton2@gmail.com>
2015-07-21 23:39:47 -0400

    Make the showcase image setup more responsive
    The general scheme is that there is an image that is as wide as the page
    and the content is absolutely positioned on the page below it. There is
    javascript in place that adjust the top of the content on load and on
    window resize to make the very top of the content block visible.
    
    The adjusting of the content block is required so that people are more
    adeqately aware that there is content to scroll to.

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -25,3 +25,19 @@ $('.song-list-item').click(function (event) {
     $(event.currentTarget).find('span').toggleClass('glyphicon-chevron-down');
     $(event.currentTarget).find('.song-list-video-placeholder').replaceWith('<iframe width="420" height="315" src="' + video.data('src') + '" frameborder="0" allowfullscreen></iframe>')
 });
+
+function adjustContentsTop()
+{
+    var showcaseHeight = $('img.showcase').outerHeight();
+    var windowHeight = $(window).height();
+    var heightDifference = showcaseHeight - windowHeight;
+    if (heightDifference >= 0) { // If showcase is taller than the window
+        $('#contents > .container').css({top: windowHeight * .9 + "px"});
+    } else {
+        $('#contents > .container').css({top: showcaseHeight + "px"});
+    }
+    $('body').scrollTop(0);
+}
+
+$(window).load(adjustContentsTop);
+$(window).resize(adjustContentsTop);
