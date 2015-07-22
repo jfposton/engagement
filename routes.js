f25497d5f5154db1e1299cf02d82e4aa4b82d003
Jonathan Poston <jfposton2@gmail.com>
2015-07-21 23:34:05 -0400

    Move routes to root
    Since we won't be having very complicated routes I'm consolidating all
    the routes to one file. This is really just to simplify the writing and
    fetching of routes.

---
+++ routes.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var model = require('./model');
+var router = express.Router();
+
+function renderTemplate(template) {
+    return function(req, res, next) {
+        res.render(template, model(template));
+    }
+}
+
+router.get(/^\/(story)?$/, renderTemplate('story'));
+
+router.get('/bride', renderTemplate('bride'));
+
+module.exports = router;
