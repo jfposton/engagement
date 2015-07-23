8fe43cc4cb95598919b7c520ae340714ab9a0a3a
Jonathan Poston <jfposton2@gmail.com>
2015-07-22 21:00:58 -0400

    Improve the screen allocation adjustment function
    Improve the function that allocates the screen to the various components
    so that if the difference between the page height is just barely enough
    to cause the top of the contents div to not be adjusted it actually is
    since otherwise one could not see the contents as intended.

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -20,10 +20,16 @@ initHeadroom();
 
 $('.song-list-item').click(function (event) {
     event.preventDefault();
-    video = $(event.currentTarget).find('.song-list-video');
+    var video = $(event.currentTarget).find('.song-list-video');
     video.toggleClass("hidden");
     $(event.currentTarget).find('span').toggleClass('glyphicon-chevron-down');
-    $(event.currentTarget).find('.song-list-video-placeholder').replaceWith('<iframe width="420" height="315" src="' + video.data('src') + '" frameborder="0" allowfullscreen></iframe>')
+    var maxHeight = 315;
+    var maxWidth = 420;
+    var expectedRatio = maxWidth / height;
+    var windowWidth = $(window).width();
+    var width = maxWidth > windowWidth ? windowWidth - 100 : maxWidth;
+    var height = width < maxWidth ? maxHeight / expectedRatio : maxHeight;
+    $(event.currentTarget).find('.song-list-video-placeholder').replaceWith('<iframe width="' + width +'" height="' + height + '" src="' + video.data('src') + '" frameborder="0" allowfullscreen></iframe>')
 });
 
 function adjustContentsTop()
@@ -31,7 +37,7 @@ function adjustContentsTop()
     var showcaseHeight = $('img.showcase').outerHeight();
     var windowHeight = $(window).height();
     var heightDifference = showcaseHeight - windowHeight;
-    if (heightDifference >= 0) { // If showcase is taller than the window
+    if (heightDifference >= -5) { // If showcase is taller than the window. -5 to allow some room for error
         $('#contents > .container').css({top: windowHeight * .9 + "px"});
     } else {
         $('#contents > .container').css({top: showcaseHeight + "px"});
