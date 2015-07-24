f5a4a9db0f064fed8f2adac5df391bb89ea870bf
Jonathan Poston <jfposton2@gmail.com>
2015-07-23 23:42:42 -0400

    Harden launchForever.js

--- launchForever.js
+++ launchForever.js
@@ -1,17 +1,16 @@
 var forever = require('forever-monitor');
-
-var child = new (forever.Monitor)('./bin/www', {
-    max: 3,
-    silent: true,
-    args: [],
-    env: {'PORT': '8080'},
+var startFile = __dirname + '/bin/www';
+var child = new (forever.Monitor)(startFile, {
+    max: 10,
+    silent: false,
+    env: {'PORT': '80'},
     logFile: '/var/log/forever/forever.log',
     outFile: '/var/log/forever/outfile.log',
     errFile: '/var/log/forever/errFile.log',
 });
 
 child.on('exit', function () {
-    console.log('your-filename.js has exited after 3 restarts');
+    console.log(startFile + ' has exited after 3 restarts');
 });
 child.on('watch:restart', function(info) {
     console.error('Restaring script because ' + info.file + ' changed');
