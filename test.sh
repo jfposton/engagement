ba8e16c16bbf2e01f490cfa949a7de26064b8123
Jonathan Poston <jfposton2@gmail.com>
2015-09-23 01:09:13 -0400

    Add in a "does npm start work" test

--- test.sh
+++ test.sh
@@ -8,6 +8,19 @@ if [ "$RET" != "0" ]; then
 fi
 echo "Running jasmine"
 jasmine
+RET=$?
 if [ "$RET" != "0" ]; then
+    exit 1
+fi
+
+which timeout
+RET=$?
+if [ "$RET" != "0" ]; then
+    exit 0
+fi
+
+timeout 10s $(which npm) start
+RET=$?
+if [ "$RET" != "124" ]; then
     exit 1
 fi
