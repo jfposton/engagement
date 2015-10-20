4f8a9660a7838bd52bc2435111a32f566f57085d
Jonathan Poston <jfposton2@gmail.com>
2015-10-19 23:29:38 -0400

    Uncomment version protection code
    The zombie tests need to be protected to ensure they are only run on the
    correct version.

--- spec/engagement/GUITesting.js
+++ spec/engagement/GUITesting.js
@@ -1,8 +1,8 @@
-//var semver = require("semver");
-//if (true) { //!semver.satisfies(process.version, '^1.6.0')) {
-    //console.log("Can't execute zombie tests due to node version requirements");
-    //return;
-//}
+var semver = require("semver");
+if (true) { //!semver.satisfies(process.version, '^1.6.0')) {
+    console.log("Can't execute zombie tests due to node version requirements");
+    return;
+}
 var Browser = require("zombie");
 var port = process.env.NODE_ENV === 'production' ? '8080' : '3000';
 var base_url = "http://localhost:" + port;
